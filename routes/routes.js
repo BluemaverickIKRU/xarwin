75ca0877cea54172f350de40543d3178cc3000e8
rajasekar <rajasekarb2917@gmail.com>
2022-10-01 19:51:04 +0530

    Added files

--- routes/routes.js
+++ routes/routes.js
@@ -9,7 +9,7 @@ const {
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('Welcome to record system API !');
+  res.send('Welcome to Record System API !');
 });
 
 router.get('/getRecord', async (req, res) => {
